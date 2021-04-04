a65ecaabdce5dac9b4ca58977fedc6294c1d4a0f
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2021-04-04 16:51:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinIncaltaminte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Incaltaminte Adidas = new Incaltaminte("Adidas,150,5,true,model 2019,#25RF7");
+            Incaltaminte Nike = new Incaltaminte("Nike", 160, 6, true, "Model 2020","#25RF6");
+            Console.WriteLine(Adidas.ToString());
+            Console.WriteLine(Nike.ToString());
+            Console.ReadKey();
+        }
+    }
+}
