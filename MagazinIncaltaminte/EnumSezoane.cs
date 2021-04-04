9d1dd59fbb7ee5b9dc472c7e6406c529f42ff401
SebiPrelipcean <62348238+SebiPrelipcean@users.noreply.github.com>
2021-04-04 17:01:27 +0300

    Add files via upload

---
+++ MagazinIncaltaminte/EnumSezoane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinIncaltaminte
+{
+    public enum EnumSezoane
+    {
+        Primavara = 1,
+        Vara = 2,
+        Toamna = 3,
+        Iarna = 4
+    };
+}
---
+++ MagazinIncaltaminte/Incaltaminte.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinIncaltaminte
+{
+    public class Incaltaminte
+    {
+        public string Denumire { get; set; }
+        public float Pret { get; set; }
+        public int Bucati { get; set; }
+        public bool Disponibil { get; set; }
+        public string Detalii { get; set; }
+        public string Codprodus { get; set; }
+        public string gen { get; set; }
+
+
+        public Incaltaminte(string date)
+        {
+            string[] aux = date.Split(',');
+            Denumire = aux[0];
+            Pret = Convert.ToInt32(aux[1]);
+            Bucati = Convert.ToInt32(aux[2]);
+            Disponibil = Convert.ToBoolean(aux[3]);
+            Detalii = aux[4];
+            Codprodus = aux[5];
+
+        }
+
+        public Incaltaminte(string _den, float _pret,int _buc,bool _dis, string _det, string _cod )
+        {
+            this.Denumire = _den;
+            this.Pret = _pret;
+            this.Bucati = _buc;
+            this.Disponibil = _dis;
+            this.Detalii = _det;
+            this.Codprodus = _cod;
+        }
+
+        public override string ToString()
+        {
+            return "Denumire: " + Denumire + "\nPret: " + Pret + "\nBucati: " + Bucati + "\nDisponibil: " + Disponibil + "\nDetalii: " + Detalii + "\nCod Produs: " + Codprodus;
+        }
+
+        public string ConversieLaSir()
+        {
+            return string.Format(Denumire + "," + Pret + "," + Bucati + "," + Disponibil + "," + Detalii + "," + Codprodus + "\n");
+        }
+    }
+
+
+}
